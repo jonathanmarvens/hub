5da506e92c07f525633cb14bcb5f10dc1124ae53
Jingwen Owen Ou <jingweno@gmail.com>
2014-04-10 21:48:38 -0700

    Add protocol to hub config

--- github/configs.go
+++ github/configs.go
@@ -21,6 +21,7 @@ type Host struct {
 	Host        string `toml:"host"`
 	User        string `toml:"user"`
 	AccessToken string `toml:"access_token"`
+	Protocol    string `toml:"protocol"`
 }
 
 type Configs struct {
